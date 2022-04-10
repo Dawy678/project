bf2ccd8cb9cf7a58479e71b62f5955fcdecd2ac4
Dawy678 <dawios2006@wp.pl>
2022-04-10 20:08:21 +0200

    2.3nd

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ window.onscroll = function() {
     progress.style.height = progressHeight + "%";
 }
 //
+// rain
+
+function rain() {
+    let amount = 200;
+    let body = document.getElementById('rain');
+    let i = 0;
+    while(i < amount) {
+        let drop = document.createElement('i');
+        drop.setAttribute('class', 'raindrop');
+        let size = Math.random() * 5;
+        let posX = Math.floor(Math.random() * window.innerWidth);
+        let delay = Math.random() * -20;
+        let duration = Math.random() * 5;
+        
+        drop.style.width = 0.2 + size+'px';
+        drop.style.left = posX + 'px';
+        drop.style.animationDelay = delay + 's';
+        drop.style.animationDuration = 1 + duration + 's';
+        body.appendChild(drop);
+        i++
+    }
+}
+rain();
